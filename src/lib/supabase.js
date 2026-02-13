5e7c3ede214b2041eddcfcb6f55cb33f774c6161
Daniel Dsiminenko <Dsiminenko05@gmail.com>
2026-02-13 16:17:35 +0000

    Add Supabase credentials

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://xydpshxaajsijqjjhasb.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5ZHBzaHhhYWpzaWpxampoYXNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5ODkzMzksImV4cCI6MjA4NjU2NTMzOX0.hW2fwRP19lWkGn5L0hDKLQXRKrl3iZn8ex2Mxq03nug';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
